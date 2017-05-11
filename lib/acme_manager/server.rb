ac04decfc984226df4aeddef06f3b38fa64c9890
Charlie Smurthwaite <charlie@atechmedia.com>
2017-05-11 16:01:25 +0100

    prefix URLs for forwardability

--- lib/acme_manager/server.rb
+++ lib/acme_manager/server.rb
@@ -4,13 +4,13 @@ module AcmeManager
   class Server
     def call(env)
       case env['REQUEST_PATH']
-      when '/'
+      when '/~acmemanager/list'
         [200, {'Content-Type' => 'text/plain'}, [AcmeManager.certificates.to_json]]
-      when /\/issue\/(.+)/
+      when /\A\/~acmemanager\/issue\/(.+)/
         domain = $1
         response = {:result => Certificate.issue(domain)}
         [200, {'Content-Type' => 'text/plain'}, [response.to_json]]
-      when /\/.well-known\/acme-challenge\/(.+)/
+      when /\A\/.well-known\/acme-challenge\/(.+)/
         token = $1
         [200, {'Content-Type' => 'text/plain'}, [File.read(File.join('data', 'challenges', token))]]
       end
